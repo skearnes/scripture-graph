0677c273c6d825b1fc1de680b604f520741e32e3
Steven Kearnes <skearnes@gmail.com>
2020-12-20 14:16:49 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+def main():
+    setup(name='scripture-graph', packages=find_packages())
+
+
+if __name__ == '__main__':
+    main()
