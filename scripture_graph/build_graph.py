e8832281253cbb028b77b0874bd514346c0c3e12
Steven Kearnes <skearnes@gmail.com>
2020-12-20 22:52:42 -0700

    Update build_graph.py

--- scripture_graph/build_graph.py
+++ scripture_graph/build_graph.py
@@ -1,19 +1,33 @@
 """Builds a scripture graph."""
 
+import glob
+
 from absl import app
 from absl import flags
+from absl import logging
 
 from scripture_graph import graph_lib
 
 FLAGS = flags.FLAGS
-flags.DEFINE_string('input', None, 'Input EPUB.')
+flags.DEFINE_string('input_pattern', None, 'Input EPUB pattern.')
 
 
 def main(argv):
     del argv  # Only used by app.run().
-    verses, references = graph_lib.read_epub(FLAGS.input)
+    verses = {}
+    references = []
+    for filename in glob.glob(FLAGS.input_pattern):
+        logging.info(filename)
+        this_verses, this_references = graph_lib.read_epub(filename)
+        logging.info(
+            f'Found {len(this_verses)} verses and'
+            f' {len(this_references)} references')
+        verses.update(this_verses)
+        references.extend(this_references)
+    logging.info(
+        f'Found {len(verses)} verses and {len(references)} references')
 
 
 if __name__ == '__main__':
-    flags.mark_flag_as_required('input')
+    flags.mark_flag_as_required('input_pattern')
     app.run(main)
