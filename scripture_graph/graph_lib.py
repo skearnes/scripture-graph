52b66fbfc83108d57ba8db2853b27cbf378196cc
Steven Kearnes <skearnes@gmail.com>
2021-03-27 19:51:07 -0600

    factor out names

--- scripture_graph/graph_lib.py
+++ scripture_graph/graph_lib.py
@@ -30,130 +30,10 @@ import numpy as np
 import pandas as pd
 import tensorflow_hub as hub
 
+import scripture_graph
+
 # pylint: disable=too-many-branches
 # pylint: disable=too-many-locals
-
-# Short names for books (used in references).
-BOOKS_SHORT = {
-    '1 Chronicles': '1 Chr.',
-    '1 Corinthians': '1 Cor.',
-    '1 John': '1 Jn.',
-    '1 Kings': '1 Kgs.',
-    '1 Peter': '1 Pet.',
-    '1 Samuel': '1 Sam.',
-    '1 Thessalonians': '1 Thes.',
-    '1 Timothy': '1 Tim.',
-    '2 Chronicles': '2 Chr.',
-    '2 Corinthians': '2 Cor.',
-    '2 John': '2 Jn.',
-    '2 Kings': '2 Kgs.',
-    '2 Peter': '2 Pet.',
-    '2 Samuel': '2 Sam.',
-    '2 Thessalonians': '2 Thes.',
-    '2 Timothy': '2 Tim.',
-    '3 John': '3 Jn.',
-    'Acts': 'Acts',
-    'Amos': 'Amos',
-    'Colossians': 'Col.',
-    'Daniel': 'Dan.',
-    'Deuteronomy': 'Deut.',
-    'Ecclesiastes': 'Eccl.',
-    'Ephesians': 'Eph.',
-    'Esther': 'Esth.',
-    'Exodus': 'Ex.',
-    'Ezekiel': 'Ezek.',
-    'Ezra': 'Ezra',
-    'Galatians': 'Gal.',
-    'Genesis': 'Gen.',
-    'Habakkuk': 'Hab.',
-    'Haggai': 'Hag.',
-    'Hebrews': 'Heb.',
-    'Hosea': 'Hosea',
-    'Isaiah': 'Isa.',
-    'James': 'James',
-    'Jeremiah': 'Jer.',
-    'Job': 'Job',
-    'Joel': 'Joel',
-    'John': 'John',
-    'Jonah': 'Jonah',
-    'Joshua': 'Josh.',
-    'Jude': 'Jude',
-    'Judges': 'Judg.',
-    'Lamentations': 'Lam.',
-    'Leviticus': 'Lev.',
-    'Luke': 'Luke',
-    'Malachi': 'Mal.',
-    'Mark': 'Mark',
-    'Matthew': 'Matt.',
-    'Micah': 'Micah',
-    'Nahum': 'Nahum',
-    'Nehemiah': 'Neh.',
-    'Numbers': 'Num.',
-    'Obadiah': 'Obad.',
-    'Philemon': 'Philem.',
-    'Philippians': 'Philip.',
-    'Proverbs': 'Prov.',
-    'Psalms': 'Ps.',
-    'Revelation': 'Rev.',
-    'Romans': 'Rom.',
-    'Ruth': 'Ruth',
-    'Song of Solomon': 'Song.',
-    'Titus': 'Titus',
-    'Zechariah': 'Zech.',
-    'Zephaniah': 'Zeph.',
-    '1 Nephi': '1 Ne.',
-    '2 Nephi': '2 Ne.',
-    '3 Nephi': '3 Ne.',
-    '4 Nephi': '4 Ne.',
-    'Alma': 'Alma',
-    'Enos': 'Enos',
-    'Ether': 'Ether',
-    'Helaman': 'Hel.',
-    'Jacob': 'Jacob',
-    'Jarom': 'Jarom',
-    'Mormon': 'Morm.',
-    'Moroni': 'Moro.',
-    'Mosiah': 'Mosiah',
-    'Omni': 'Omni',
-    'Words of Mormon': 'W of M',
-    'Doctrine and Covenants': 'D&C',
-    'Articles of Faith': 'A of F',
-    'Abraham': 'Abr.',
-    'Joseph Smith—Matthew': 'JS—M',
-    'Joseph Smith—History': 'JS—H',
-    'Moses': 'Moses',
-}
-
-# Volumes of scripture.
-VOLUMES = {
-    'Old Testament': [
-        'Gen.', 'Ex.', 'Lev.', 'Num.', 'Deut.', 'Josh.', 'Judg.', 'Ruth',
-        '1 Sam.', '2 Sam.', '1 Kgs.', '2 Kgs.', '1 Chr.', '2 Chr.', 'Ezra',
-        'Neh.', 'Esth.', 'Job', 'Ps.', 'Prov.', 'Eccl.', 'Song.', 'Isa.',
-        'Jer.', 'Lam.', 'Ezek.', 'Dan.', 'Hosea', 'Joel', 'Amos', 'Obad.',
-        'Jonah', 'Micah', 'Nahum', 'Hab.', 'Zeph.', 'Hag.', 'Zech.', 'Mal.'
-    ],
-    'New Testament': [
-        'Matt.', 'Mark', 'Luke', 'John', 'Acts', 'Rom.', '1 Cor.', '2 Cor.',
-        'Gal.', 'Eph.', 'Philip.', 'Col.', '1 Thes.', '2 Thes.', '1 Tim.',
-        '2 Tim.', 'Titus', 'Philem.', 'Heb.', 'James', '1 Pet.', '2 Pet.',
-        '1 Jn.', '2 Jn.', '3 Jn.', 'Jude', 'Rev.'
-    ],
-    'Book of Mormon': [
-        '1 Ne.', '2 Ne.', 'Jacob', 'Enos', 'Jarom', 'Omni', 'W of M', 'Mosiah',
-        'Alma', 'Hel.', '3 Ne.', '4 Ne.', 'Morm.', 'Ether', 'Moro.'
-    ],
-    'Doctrine and Covenants': ['D&C'],  # Note that 'OD' is excluded.
-    'Pearl of Great Price': ['Moses', 'Abr.', 'JS—M', 'JS—H', 'A of F'],
-    'Study Helps': ['BD', 'HC', 'JST', 'TG', 'ITC'],
-}
-VOLUMES_SHORT = {
-    'Old Testament': 'OT',
-    'New Testament': 'NT',
-    'Book of Mormon': 'BoM',
-    'Doctrine and Covenants': 'D&C',
-    'Pearl of Great Price': 'PoGP',
-}
 
 # XML namespaces.
 NAMESPACES = {'default': 'http://www.w3.org/1999/xhtml'}
@@ -161,7 +41,7 @@ NAMESPACES = {'default': 'http://www.w3.org/1999/xhtml'}
 
 def get_volume(book: str) -> str:
     """Returns the containing volume for a book."""
-    for volume, books in VOLUMES.items():
+    for volume, books in scripture_graph.VOLUMES.items():
         if book in books:
             return volume
     raise ValueError(f'unrecognized book: {book}')
@@ -281,7 +161,7 @@ def read_headers(tree) -> Tuple[Optional[str], Optional[int]]:
     title = get_title(tree)
     book = title.split('Chapter')[0].split('Section')[0].split(
         'Psalm ')[0].strip()
-    book_short = BOOKS_SHORT[book]
+    book_short = scripture_graph.BOOKS_SHORT[book]
     title_number = cssselect.CSSSelector('.titleNumber')(tree)
     if not title_number:
         return None, None  # Table of contents, etc.
@@ -409,7 +289,7 @@ def parse_reference(text: str) -> List[str]:
             if not chapter_verse.strip():
                 continue
             book = match[0].strip()
-            if book not in BOOKS_SHORT.values():
+            if book not in scripture_graph.BOOKS_SHORT.values():
                 if not book.startswith(skipped):
                     raise ValueError(
                         f'unrecognized reference to book: "{book}" ({text})')
@@ -625,9 +505,11 @@ def write_tree(graph: nx.Graph, filename: str):
     graph = graph.copy()
     remove_topic_nodes(graph)
     source = []
-    book_names = {value: key for key, value in BOOKS_SHORT.items()}
-    for volume, books in VOLUMES.items():
-        if volume not in VOLUMES_SHORT:
+    book_names = {
+        value: key for key, value in scripture_graph.BOOKS_SHORT.items()
+    }
+    for volume, books in scripture_graph.VOLUMES.items():
+        if volume not in scripture_graph.VOLUMES_SHORT:
             continue
         volume_children = []
         for book_short in books:
